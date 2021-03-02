3424457f7ce5f4dbd828f37e08839bf649f20434
Chrishaun Byrom <Chrishaun.byrom@outlook.com>
2021-03-02 10:00:55 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.Write a C# Sharp program to print Hello and your name in a separate line. 
+              Expected Output : Hello: Alexandra Abramov*/
+
+
+            //Solution
+
+            Console.WriteLine("Hello: Chrishaun Byrom");
+        }
+    }
+}
